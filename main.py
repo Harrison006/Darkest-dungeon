f06950c88684497a8a4735e10be0294237152611
Harrison006 <87344112+Harrison006@users.noreply.github.com>
2021-07-19 19:53:58 +1000

    Homework

--- main.py
+++ main.py
@@ -11,11 +11,16 @@ armoury.description = ("the walls are lined with racks that once help weapons an
 lab = Room("Laboratory")
 lab.description = ("A strange odour hangs in a room filled with unknownabke contraptions.")
 
+Tavern = Room("Tavern")
+Tavern.description = ("A room at the front of the game that is the main room")
+
 cavern.link_rooms(armoury,"south")
 armoury.link_rooms(cavern,"north")
 armoury.link_rooms(lab,"east")
 lab.link_rooms(armoury,"west")
+Tavern.link_rooms(cavern,"North")
 
 cavern.describe()
 armoury.describe()
 lab.describe()
+Tavern.describe()
